b89337a1de73c007f39a3043c8b9b0b7f08c4e09
Vidun2004 <hettividun@gmail.com>
2026-01-20 16:18:06 +0530

    API ADD RENDER

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getAccessToken } from "../auth/tokenStore";
 
-export const API_BASE = "http://localhost:5000/api";
+export const API_BASE = "https://otflow-backend.onrender.com/api";
 
 export const api = axios.create({
   baseURL: API_BASE,
