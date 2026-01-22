08dceefd4b916704096dea6f27549b18e3665f96
Vidun2004 <hettividun@gmail.com>
2026-01-22 11:31:22 +0530

    env addition

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getAccessToken } from "../auth/tokenStore";
 
-export const API_BASE = "https://otflow-backend.onrender.com/api";
+export const API_BASE = `${import.meta.env.VITE_API_URL}/api`;
 
 export const api = axios.create({
   baseURL: API_BASE,
