2364cbb1c2e786c53b727b948a1b25ef6dee2c9a
Vidun Hettiarachchi <s23010812@ousl.lk>
2026-01-25 17:58:11 +0530

    fix

--- src/components/ui/StateLoader.tsx
+++ src/components/ui/StateLoader.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/set-state-in-effect */
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { motion } from "framer-motion";
 import {
   RefreshCw,
